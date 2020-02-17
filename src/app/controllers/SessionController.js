4529ec6219010b0e1515874abfa92538dd174c76
Matheus <matheus.afornali@gobrax.com>
2020-02-17 16:18:32 -0300

    - Iniciado login com JWT. Signed-off-by: Matheus <afornalimatheus@gmail.com>

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+class SessionController {
+  store() {
+    console.log('HELLOOO AQUI');
+    return false;
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.status(400).json({ msg: 'OK' });
-});
+routes.get('/session', SessionController.store);
 
 export default routes;
